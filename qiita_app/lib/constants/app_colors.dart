fc2aba4b4a21526ef96ad20e4b9c2f64aafeeff2
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-02-11 00:05:57 +0900

    cherry-pickでapp_colorを追加

--- qiita_app/lib/constants/app_colors.dart
+++ qiita_app/lib/constants/app_colors.dart
@@ -5,5 +5,9 @@ class AppColors {
   static const Color darkgreen = Color.fromRGBO(70, 131, 0, 1);
   static const secondary = Color.fromRGBO(130, 130, 130, 1);
   static const white = Colors.white;
+
   static const black = Colors.black;
+
+  static const background = Color(0xFFFAFAFA);
+  static const divider = Color(0xFFE0E0E0);
 }
