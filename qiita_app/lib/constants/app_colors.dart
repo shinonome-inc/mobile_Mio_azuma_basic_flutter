15025416c713299ba2b037b79b2c5b4e2b3e6d5a
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-02-11 00:05:32 +0900

    AppColorsからbackgroundとdividerを削除

--- qiita_app/lib/constants/app_colors.dart
+++ qiita_app/lib/constants/app_colors.dart
@@ -5,6 +5,4 @@ class AppColors {
   static const Color darkgreen = Color.fromRGBO(70, 131, 0, 1);
   static const secondary = Color.fromRGBO(130, 130, 130, 1);
   static const white = Colors.white;
-  static const background = Color(0xFFFAFAFA);
-  static const divider = Color(0xFFE0E0E0);
 }
