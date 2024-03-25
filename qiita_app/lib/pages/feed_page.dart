32f2ad8011777fe6c2796ca472059ce08e4ab602
iwase_mio <mio.disney110@gmail.com>
2024-03-25 17:22:22 +0900

    修正

--- qiita_app/lib/pages/feed_page.dart
+++ qiita_app/lib/pages/feed_page.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:qiita_app/constants/app_text_style.dart';
 import 'package:qiita_app/models/article.dart';
 import 'package:qiita_app/repository/qiita_repository.dart';
 import 'package:qiita_app/widgets/app_title.dart';
 import 'package:qiita_app/widgets/article_container.dart';
-import 'package:flutter/cupertino.dart';
 
 class FeedPage extends StatefulWidget {
   const FeedPage({Key? key}) : super(key: key);
-
   @override
   State<FeedPage> createState() => _FeedPageState();
 }
@@ -18,10 +15,6 @@ class _FeedPageState extends State<FeedPage> {
   List<Article> articles = [];
   bool isLoading = false; // データ読み込み中かどうかを示すフラグを追加
   int currentPage = 1;
-  // テキストフィールドの入力を管理する
-  final TextEditingController _searchController = TextEditingController();
-  bool _isLoading = false;
-
   @override
   void initState() {
     super.initState();
@@ -50,38 +43,15 @@ class _FeedPageState extends State<FeedPage> {
       currentPage++; // 次のページ番号を更新
       fetchArticles();
     }
-    
-  @override
-  void dispose() {
-    _searchController.dispose(); // ウィジェットが破棄される際にTextEditingControllerをクリーンアップ
-    super.dispose();
-  }
-
-  List<Article> articles = [];
-
-  void fetchArticles({String query = ''}) async {
-    setState(() {
-      _isLoading = true; // ローディング開始
-    });
-
-    // QiitaRepositoryから記事データを非同期で取得
-    List<Article> fetchedArticles =
-        await QiitaRepository.fetchQiitaItems(query: query);
-    // 取得した記事データをステートにセット
-    setState(() {
-      articles = fetchedArticles;
-      _isLoading = false; // ローディング終了
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppTitle(
+      appBar: const AppTitle(
         title: 'Feed',
         showSearchBar: true,
         showBottomDivider: true,
-
       ),
       body: ListView.builder(
         controller: _scrollController, // スクロールコントローラーを設定
@@ -90,47 +60,8 @@ class _FeedPageState extends State<FeedPage> {
           return ArticleContainer(
             article: articles[index],
           );
-
-        searchController: _searchController,
-        onSearch: (query) {
-          final searchQuery = QiitaRepository.buildQuery(query);
-          fetchArticles(query: searchQuery);
-
         },
       ),
-      body: Builder(builder: (context) {
-        if (_isLoading) {
-          return const Center(
-            child: CupertinoActivityIndicator(),
-          );
-        } else if (articles.isEmpty && _searchController.text.isNotEmpty) {
-          return const Center(
-            child: Column(
-              children: [
-                SizedBox(height: 228),
-                Text(
-                  '検索にマッチする記事はありませんでした',
-                  style: AppTextStyles.h2BasicBlack,
-                ),
-                SizedBox(height: 18),
-                Text(
-                  '検索条件を変えるなどして再度検索をしてください',
-                  style: AppTextStyles.h3BasicSecondary,
-                )
-              ],
-            ),
-          );
-        } else {
-          return ListView.builder(
-            itemCount: articles.length,
-            itemBuilder: (context, index) {
-              return ArticleContainer(
-                article: articles[index],
-              );
-            },
-          );
-        }
-      }),
     );
   }
 
--- qiita_app/lib/repository/qiita_repository.dart
+++ qiita_app/lib/repository/qiita_repository.dart
@@ -26,26 +26,10 @@ class QiitaRepository {
     }
   }
 
-
   static Future<List<Article>> fetchQiitaItems(int page) async {
     final url = Uri.parse('${Urls.qiitaBaseUrl}/items?page=$page');
-
-  static Future<List<Article>> fetchQiitaItems({String query = ''}) async {
-    Uri url; // Uriオブジェクトの初期宣言
-    if (query.isNotEmpty) {
-      // 検索クエリが空でない場合、クエリパラメータを含むURLを構築（エンコードなし）
-      url = Uri.parse('${Urls.qiitaBaseUrl}/items?query=$query');
-      // // 検索クエリが空でない場合、クエリパラメータを含むURLを構築（エンコードあり）
-      // url = Uri.parse(
-      //     '${Urls.qiitaBaseUrl}/items?query=${Uri.encodeComponent(query)}');
-    } else {
-      // 検索クエリが空の場合、クエリパラメータを含まないURLを構築
-      url = Uri.parse('${Urls.qiitaBaseUrl}/items');
-    }
-
     try {
       final response = await http.get(url);
-
       if (response.statusCode == 200) {
         final List<dynamic> jsonResponse = jsonDecode(response.body);
         return jsonResponse.map((data) => Article.fromJson(data)).toList();
@@ -57,32 +41,6 @@ class QiitaRepository {
       // 例外が発生した場合のエラーハンドリング
       throw Exception('Failed to load Qiita items: $e');
     }
-  }
-
-  static buildQuery(String input) {
-    // 簡単な前処理: トリミングして、空白で単語に分割
-    var words = input.trim().split(RegExp(r"\s+"));
-
-    // クエリ部品を格納するリスト
-    List<String> queryParts = [];
-
-    // タグ、キーワードなど特定の条件に応じてクエリ部品を組み立てる
-    for (var word in words) {
-      if (word.startsWith("#")) {
-        // タグ指定の場合 (例: "#Flutter")
-        queryParts.add("tag:${word.substring(1)}");
-      } else if (word.startsWith("@")) {
-        // ユーザー指定の場合 (例: "@user")
-        queryParts.add("user:${word.substring(1)}");
-      } else {
-        // 一般的なキーワードの場合
-        // タイトル、本文、コード内にキーワードが含まれている記事を検索
-        queryParts.add("title:$word OR body:$word OR code:$word");
-      }
-    }
-
-    // クエリ部品を結合して完全なクエリを生成
-    return queryParts.join(" ");
   }
 
   static Future<List<Tag>> fetchQiitaTags() async {
