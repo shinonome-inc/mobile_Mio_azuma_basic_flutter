d5e1f2dc67e1582a345e140895668e1794fcfe9c
azuma502 <bassetchocolat79@gmail.com>
2024-04-08 15:25:57 +0900

    mypagenotloginをコメントアウト

--- qiita_app/lib/pages/my_page.dart
+++ qiita_app/lib/pages/my_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:qiita_app/constants/app_text_style.dart';
 import 'package:qiita_app/models/article.dart';
 import 'package:qiita_app/models/user.dart';
+import 'package:qiita_app/pages/my_page_notlogin.dart';
 import 'package:qiita_app/repository/qiita_repository.dart';
 import 'package:qiita_app/widgets/app_title.dart';
 import 'package:qiita_app/widgets/article_container.dart';
@@ -9,9 +9,7 @@ import 'package:qiita_app/widgets/section_divider.dart';
 import 'package:qiita_app/widgets/user_info_container.dart';
 
 class MyPage extends StatefulWidget {
-  const MyPage({
-    Key? key,
-  }) : super(key: key);
+  const MyPage({Key? key}) : super(key: key);
 
   @override
   State<MyPage> createState() => _MyPageState();
@@ -20,6 +18,7 @@ class MyPage extends StatefulWidget {
 class _MyPageState extends State<MyPage> {
   List<Article> articles = [];
   User? loggedInUser;
+  bool isLoading = true;  // ローディング状態の管理
 
   @override
   void initState() {
@@ -28,79 +27,58 @@ class _MyPageState extends State<MyPage> {
   }
 
   Future<void> fetchLoggedInUserInfo() async {
-    debugPrint('Starting to fetch logged in user info...');
+    setState(() {
+      isLoading = true;  // ローディング開始
+    });
 
     try {
       User userInfo = await QiitaRepository.fetchAuthenticatedUserInfo();
-      debugPrint('Logged in user info fetched successfully.');
-
       List<Article> userArticles = await QiitaRepository.fetchUserArticles(userInfo.id);
 
-      setState(() {
-        loggedInUser = userInfo;
-        articles = userArticles;  // ユーザーの記事リストを更新
-      });
+      if (mounted) {
+        setState(() {
+          loggedInUser = userInfo;
+          articles = userArticles;
+          isLoading = false;  // ローディング終了
+        });
+      }
     } catch (e) {
       debugPrint('Failed to fetch user info: $e');
+      if (mounted) {
+        setState(() {
+          isLoading = false;  // エラー時もローディング終了
+        });
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const AppTitle(
-        title: 'MyPage',
-        showBottomDivider: true,
-      ),
-      body: Column(
-        children: [
-          if (loggedInUser != null) ...[
-            UserInfoContainer(
-              user: loggedInUser!,
-            )
-          ] else ...[
-            const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CircleAvatar(
-                  backgroundImage: NetworkImage('https://unsplash.it/630/400'),
-                  radius: 40,
-                ),
-                SizedBox(height: 16),
-                Text(
-                  'username',
-                  style: AppTextStyles.h2BasicBlack,
-                ),
-                SizedBox(height: 4),
-                Text(
-                  '@userID',
-                  style: AppTextStyles.h3BasicSecondary,
-                ),
-                Text(
-                  'User Description User Description User Description User Description',
-                  maxLines: 3,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                SizedBox(height: 4),
-                Text('100フォロー'),
-                Text('100フォロワー'),
-              ],
+    if (isLoading) {
+      return const Scaffold(
+        appBar:  AppTitle(title: 'MyPage', showBottomDivider: true),
+        body:  Center(child: CircularProgressIndicator()),  // ローディングインジケーターを表示
+      );
+    // } else if (loggedInUser == null) {
+    //   return const MyPageNotLogin();
+    } else {
+      return Scaffold(
+        appBar: const AppTitle(title: 'MyPage', showBottomDivider: true),
+        body: Column(
+          children: [
+            UserInfoContainer(user: loggedInUser!),
+            const SectionDivider(text: '投稿記事'),
+            Expanded(
+              child: ListView.builder(
+                itemCount: articles.length,
+                itemBuilder: (context, index) {
+                  return ArticleContainer(article: articles[index]);
+                },
+              ),
             ),
           ],
-          const SectionDivider(text: '投稿記事'),
-          Expanded(
-            child: ListView.builder(
-              itemCount: articles.length,
-              itemBuilder: (context, index) {
-                return ArticleContainer(
-                  article: articles[index],
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
+        ),
+      );
+    }
   }
 }
-
