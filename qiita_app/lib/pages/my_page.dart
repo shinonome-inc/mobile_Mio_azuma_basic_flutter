d9369a984429e8e955ee1eecdd5e30559b841279
iwase_mio <mio.disney110@gmail.com>
2024-04-14 19:50:44 +0900

    myページにエラー実装

--- qiita_app/lib/pages/my_page.dart
+++ qiita_app/lib/pages/my_page.dart
@@ -4,6 +4,7 @@ import 'package:qiita_app/models/user.dart';
 import 'package:qiita_app/repository/qiita_repository.dart';
 import 'package:qiita_app/widgets/app_title.dart';
 import 'package:qiita_app/widgets/article_container.dart';
+import 'package:qiita_app/widgets/network_error.dart';
 import 'package:qiita_app/widgets/section_divider.dart';
 import 'package:qiita_app/widgets/user_info_container.dart';
 
@@ -17,7 +18,8 @@ class MyPage extends StatefulWidget {
 class _MyPageState extends State<MyPage> {
   List<Article> articles = [];
   User? loggedInUser;
-  bool isLoading = true; // ローディング状態の管理
+  bool isLoading = true;
+  bool hasError = false;
 
   @override
   void initState() {
@@ -27,7 +29,7 @@ class _MyPageState extends State<MyPage> {
 
   Future<void> fetchLoggedInUserInfo() async {
     setState(() {
-      isLoading = true; // ローディング開始
+      isLoading = true;
     });
 
     try {
@@ -39,7 +41,8 @@ class _MyPageState extends State<MyPage> {
         setState(() {
           loggedInUser = userInfo;
           articles = userArticles;
-          isLoading = false; // ローディング終了
+          isLoading = false;
+          hasError = false;
         });
       }
     } catch (e) {
@@ -47,9 +50,9 @@ class _MyPageState extends State<MyPage> {
       if (mounted) {
         setState(() {
           loggedInUser = null;
-          isLoading = false; // エラー時もローディング終了
+          isLoading = false;
+          hasError = true;
         });
-        throw Exception('Failed to load Qiita items: $e');
       }
     }
   }
@@ -60,6 +63,18 @@ class _MyPageState extends State<MyPage> {
       return const Scaffold(
         appBar: AppTitle(title: 'MyPage', showBottomDivider: true),
         body: Center(child: CircularProgressIndicator()),
+      );
+    } else if (hasError) {
+      return Scaffold(
+        appBar: const AppTitle(title: 'MyPage', showBottomDivider: true),
+        body: NetworkError(
+          onPressReload: () {
+            setState(() {
+              hasError = false;
+            });
+            fetchLoggedInUserInfo();
+          },
+        ),
       );
     }
 
