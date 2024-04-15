ee2befe85cc90692f8fe3295d0b2e41d4b49f4bf
azuma502 <bassetchocolat79@gmail.com>
2024-04-15 13:49:18 +0900

    余計なimport文を削除

--- qiita_app/lib/widgets/follow_container.dart
+++ qiita_app/lib/widgets/follow_container.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter/rendering.dart';
 import 'package:qiita_app/constants/app_colors.dart';
 import 'package:qiita_app/constants/app_text_style.dart';
 import 'package:qiita_app/models/user.dart';
@@ -9,32 +8,22 @@ class FollowContainer extends StatelessWidget {
 
   const FollowContainer({
     required this.user,
-    
     Key? key,
   }) : super(key: key);
 
-   @override
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      // ユーザー画面へ
-      // onTap: (() {
-      //   Navigator.push(
-      //     context, 
-      //     MaterialPageRoute(
-      //       builder: ((context) => userPage(),))
-
-      //     ),
-      // }
       child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 20,vertical: 8),
-        padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 8),
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
         decoration: BoxDecoration(
           border: Border.all(color: AppColors.divider),
           borderRadius: BorderRadius.circular(10),
         ),
-        child:  Column(
+        child: Column(
           children: [
-             Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 CircleAvatar(
@@ -49,40 +38,36 @@ class FollowContainer extends StatelessWidget {
                     Text(
                       user.name,
                       style: AppTextStyles.h3BasicBlack,
-                      ),
-                      Text(
-                        user.id,
-                        style: AppTextStyles.h3BasicSecondary,
-                        ),
+                    ),
+                    Text(
+                      user.id,
+                      style: AppTextStyles.h3BasicSecondary,
+                    ),
                   ],
                 ),
-               
               ],
-              
             ),
             const SizedBox(height: 4),
-             Column(
+            Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      'Posts:${user.itemsCount}',
-                      style: AppTextStyles.h3BasicBlack,
-                    ),
-                    const SizedBox(height: 4),
-                    Text(
-                      user.description,
-                      style: AppTextStyles.h3BasicSecondary,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      ),
-                      
-                  ],
+              children: [
+                Text(
+                  'Posts:${user.itemsCount}',
+                  style: AppTextStyles.h3BasicBlack,
                 ),
+                const SizedBox(height: 4),
+                Text(
+                  user.description,
+                  style: AppTextStyles.h3BasicSecondary,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
           ],
         ),
-        ),
-
+      ),
     );
   }
 }
