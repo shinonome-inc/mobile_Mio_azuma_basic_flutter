cabf1421570e4d74f352def3ec715c1105ad8453
iwase_mio <mio.disney110@gmail.com>
2024-06-02 23:22:48 +0900

    iconないときも表示できるようにした

--- qiita_app/lib/widgets/tag_container.dart
+++ qiita_app/lib/widgets/tag_container.dart
@@ -23,41 +23,43 @@ class TagContainer extends StatelessWidget {
           ),
         );
       },
-      child:Expanded(
-      child: Container(
-        margin: const EdgeInsets.all(4),
-        padding: const EdgeInsets.all(8),
-        decoration: BoxDecoration(
-          border: Border.all(color: AppColors.divider),
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // 中央寄せ
-          children: [
-            Image.network(tag.iconUrl),
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
-              child: Text(tag.id),
-            ),
-            Text(
-              '記事件数:${tag.itemsCount}',
-              style: const TextStyle(
-                fontSize: 12,
-                color: AppColors.secondary,
+      child: Expanded(
+        child: Container(
+          margin: const EdgeInsets.all(4),
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            border: Border.all(color: AppColors.divider),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, // 中央寄せ
+            children: [
+              tag.iconUrl == null
+                  ? const SizedBox.shrink()
+                  : Image.network(tag.iconUrl!),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                child: Text(tag.id),
               ),
-            ),
-            const SizedBox(height: 8.0),
-            Text(
-              'フォロワー数:${tag.followersCount}',
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(
-                fontSize: 12,
-                color: AppColors.secondary,
+              Text(
+                '記事件数:${tag.itemsCount}',
+                style: const TextStyle(
+                  fontSize: 12,
+                  color: AppColors.secondary,
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 8.0),
+              Text(
+                'フォロワー数:${tag.followersCount}',
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 12,
+                  color: AppColors.secondary,
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
       ),
     );
   }
